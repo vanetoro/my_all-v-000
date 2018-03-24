f38cf610d5d6a3b36a66510f32e24c5cb910368e
Vanessa Toro <vanetoro84@gmail.com>
2018-03-24 19:02:56 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,12 +1,16 @@
 require 'pry'
 
 def my_all?(collection)
-  my_alls = true
   i = 0
   new_arr = []
     while i < collection.length
      new_arr << yield(collection[i])
       i+=1
     end
-    new_arr
+    
+    if new_arr.include>(false)
+      false
+    else 
+      true
+    end
 end
