0527f6e36d0c73698f49a876b571826c494b001c
Vanessa Toro <vanetoro84@gmail.com>
2018-03-24 19:00:42 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,8 +2,10 @@ require 'pry'
 
 def my_all?(collection)
   i = 0
+  new_arr = []
     while i < collection.length
-      yield(collection[i])
+     new_arr << yield(collection[i])
       i+=1
     end
+    new_arr
 end
